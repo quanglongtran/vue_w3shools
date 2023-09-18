dd5e2b5af7b8766e34dfcc037cbfb154fc9a773e
quanglongtran03 <tql0928159331@gmail.com>
2023-09-18 21:57:48 +0700

    commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Food</h1>
+    <food-item />
+    <food-item />
+    <food-item />
+</template>
+
+<script></script>
+
+<style>
+#app > div:hover {
+    cursor: pointer;
+}
+</style>
